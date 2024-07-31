964db9208bbe482d00809b7fb8a67815b3379f88
jayanthkumar <jayanthkumargandhe@gmail.com>
2024-07-31 21:53:03 +0530

    forms

---
+++ backend/index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+// MongoDB Connection
+mongoose.connect('mongodb://localhost:27017/property', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+// Routes
+const propertyRoutes = require('./routes/property');
+const userRoutes = require('./routes/user');
+app.use('/api/properties', propertyRoutes);
+app.use('/api/users',userRoutes)
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// // Mock data
+// const cities = [
+//   { id: 1, name: 'Hyderabad' },
+//   { id: 2, name: 'Kamareddy' },
+//   { id: 3, name: 'Chennai' }
+// ];
+
+// const locations = {
+//   1: ['Kondapur', 'Hitech-city', 'Madhapur'],
+//   2: ['Osmanpura', 'Ashoknagar', 'vidyanagar'],
+//   3: ['Saidapet', 'Basin Bridge', 'Ayanavaram']
+// };
+
+// Endpoints
+app.get('/cities', (req, res) => {
+  res.json(cities);
+});
+
+app.get('/locations/:cityId', (req, res) => {
+  const cityId = req.params.cityId;
+  res.json(locations[cityId] || []);
+});
+
+
---
+++ backend/routes/user.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/users');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+// User Registration
+router.post('/register', async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const user = new User({ username, email, password });
+    await user.save();
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// User Login
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: 'Invalid email or password' });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Invalid email or password' });
+    }
+    const token = jwt.sign({ userId: user._id }, 'your_jwt_secret', { expiresIn: '1h' });
+    res.json({ token, userId: user._id, username: user.username });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+module.exports = router;
